af57951b4ea330e6f16fff021319fa32f4913d73
sinchanabhandary <134610755+sinchanabhandary@users.noreply.github.com>
2023-08-14 00:10:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ResumeBuilder from './Components/ResumeBuilder';
+
+function App() {
+  return (
+    <div className="App">
+      <ResumeBuilder />
+    </div>
+  );
+}
+
+export default App;
